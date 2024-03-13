1131150732d7d727be382481db0e2a1d62ab7f21
OcheOps <ochecodes@gmail.com>
2024-03-13 17:15:37 +0100

    Helpers enforcers

---
+++ api/helpers/helpers.go
@@ -0,0 +1,35 @@
+package helpers
+
+import(
+	"os"
+	"strings"
+)
+
+
+func EnforceHTTP() strings {
+	if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
+		return "http://" + url
+	}
+	return url
+}
+
+func RemoveDomain(url string) bool {
+
+	if url == os.Getenv("DOMAIN") {
+		return false
+	}
+
+
+	newURL := strings.Replace(url, "http://", "", 1)
+	newURL = strings.Replace(newURL, "https://", "", 1)
+	newURL = strings.Replace(newURL, "www.", "", 1)
+	newURL = strings.Split(newURL, "/")[0]
+
+	if newURL == os.Getenv("DOMAIN") {
+		return false
+	}
+
+	return true
+
+
+}
--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -7,17 +7,44 @@ import (
 )
 
 type request struct {
-	URL         string `json:"url"`
-	CustomShort string `json:"short"`
+	URL         string        `json:"url"`
+	CustomShort string        `json:"short"`
 	Expiry      time.Duration `json:"expiry"`
-
 }
 
 type response struct {
-	URL            string	`json:"url"`
-	CustomShort    string	`json:"short"`
+	URL            string        `json:"url"`
+	CustomShort    string        `json:"short"`
 	Expiry         time.Duration `json:"expiry"`
-	XRateRemainig  int			`json:"rate_limit"`
-	XRateLimitRest time.Duration	`json:"rate_limit_reset"`
+	XRateRemainig  int           `json:"rate_limit"`
+	XRateLimitRest time.Duration `json:"rate_limit_reset"`
+}
 
+func ShortenURL(c *fiber.Ctx) error {
+	body := new(request)
+	if err := c.BodyParser(req); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "cannot parse JSON",
+		})
+	}
+
+	//implementing ratelimiting by 1 
+
+	//check if the entry is a url 
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "invalid URL",
+		})
+	}
+
+	//check for domain errors 
+	if !helpers.RemoveDomain(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "invalid URL",
+		})
+	}
+
+	//https cheecker 
+	body.URL = helpers.EnforceHTTP(body.URL)
+	
 }
